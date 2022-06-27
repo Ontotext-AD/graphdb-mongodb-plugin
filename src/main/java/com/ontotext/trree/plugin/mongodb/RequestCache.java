e03ab175ebaba7de70a383489aa8293b5da787cf
damyan.ognyanov <damyan.ognyanov@ontotext.com>
2022-06-27 16:20:19 +0300

    fixes GDB-6954, limit up to 100 items in cache. clear currentRDF document on iterator close()

--- src/main/java/com/ontotext/trree/plugin/mongodb/RequestCache.java
+++ src/main/java/com/ontotext/trree/plugin/mongodb/RequestCache.java
@@ -5,17 +5,28 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Caches the aggregate and find calls to mongo in each request.
  */
 public class RequestCache {
 
-	protected HashMap<Integer, AggregateIterable<Document>> aggregateCache = new HashMap<>();
-	protected HashMap<Integer, FindIterable<Document>> findCache = new HashMap<>();
-
+	final int MAX_ITEMS = 100;
+	final static float FACTOR = 0.75f;
+	protected Map<Integer, AggregateIterable<Document>> aggregateCache = boundCache(MAX_ITEMS);
+	protected Map<Integer, FindIterable<Document>> findCache = boundCache(MAX_ITEMS);
+	
+	public static <K,V> Map<K,V> boundCache(final int maxSize) {
+        return new LinkedHashMap<K,V>((int)(maxSize/FACTOR), FACTOR, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<K,V> eldest) {
+            	return size() > maxSize;
+            }
+        };
+    }
 	AggregateIterable<Document> getAggregation(MongoCollection coll, String database, String collection, List<Document> aggregation) {
 		return aggregateCache
 				.computeIfAbsent(database.hashCode() ^ collection.hashCode() ^ aggregation.hashCode()
