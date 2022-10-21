70676f946b5c13b79f5b41ee4e01a4d76bd18487
Borislav-Bonev-Ontotext <111446237+Borislav-Bonev-Ontotext@users.noreply.github.com>
2022-10-21 10:49:47 +0300

    GDB-7520: Add limit to the CachingDocumentLoader

--- src/main/java/com/ontotext/trree/plugin/mongodb/CachingDocumentLoader.java
+++ src/main/java/com/ontotext/trree/plugin/mongodb/CachingDocumentLoader.java
@@ -8,10 +8,12 @@ import com.github.jsonldjava.utils.JsonUtils;
 import java.io.Closeable;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- * Custom document loader that will cache the remote documents.
+ * Custom document loader that will cache the remote documents. This will not cache regular JSON-LD
+ * documents but rather only remote {@code "@contexts": "http://remote.context.file"}
  *
  * @author <a href="mailto:borislav.bonev@ontotext.com">Borislav Bonev</a>
  * @since 19/10/2022
@@ -19,7 +21,13 @@ import java.util.Map;
 public class CachingDocumentLoader extends DocumentLoader implements Closeable {
 
   // as the document instances are immutable we can cache them whole
-  private final Map<String, RemoteDocument> documentCache = new HashMap<>();
+  private final Map<String, RemoteDocument> documentCache = new LinkedHashMap<String, RemoteDocument>() {
+    @Override protected boolean removeEldestEntry(Map.Entry eldest) {
+      // if the cache reaches 1000 elements then the first (oldest) entity will be dropped from the cache
+      // this is mainly a failsafe in case of someone tries to fill up the memory with random contexts
+      return documentCache.size() > 1000;
+    }
+  };
 
   @Override
   public RemoteDocument loadDocument(String url) throws JsonLdError {
