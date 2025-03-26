db2d5fb349212f40c694323b97028caa0d24fb8f
Borislav-Bonev-Ontotext <111446237+Borislav-Bonev-Ontotext@users.noreply.github.com>
2025-03-26 22:20:03 +0200

    Add simple batch document processing.
    Added functionality where all documents matching the input query, up to a maximum are loaded in memory and returned as named graph collection instead of processing one by one.
    When the new functionality is used the plugin is no longer streaming but will require a buffer to store the documents until the query is complete.
    
    Added system property configuration that controls the maximum allowed batch size: graphdb.mongodb.maxBatchSize. This is to prevent OOM problems.

---
+++ src/main/java/com/ontotext/trree/plugin/mongodb/BatchDocumentStore.java
@@ -0,0 +1,40 @@
+package com.ontotext.trree.plugin.mongodb;
+
+import org.eclipse.collections.api.iterator.LongIterator;
+import org.eclipse.collections.api.set.primitive.MutableLongSet;
+import org.eclipse.collections.api.factory.primitive.LongSets;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.impl.LinkedHashModel;
+
+/**
+ * Collects the document data during batch processing.
+ *
+ * @author <a href="mailto:borislav.bonev@ontotext.com">Borislav Bonev</a>
+ * @since 26/03/2025
+ */
+public class BatchDocumentStore {
+	private final MutableLongSet documentIds = LongSets.mutable.empty();
+	private final Model data = new LinkedHashModel();
+
+	public void addDocument(long id, Model model) {
+		documentIds.add(id);
+		data.addAll(model);
+	}
+
+	public Model getData() {
+		return data;
+	}
+
+	public void clear() {
+		documentIds.clear();
+		data.clear();
+	}
+
+	public int size() {
+		return documentIds.size();
+	}
+
+	LongIterator getIterator() {
+		return documentIds.longIterator();
+	}
+}
