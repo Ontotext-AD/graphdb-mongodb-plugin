744a113c4a099fd1e5ae7b3c12530b6ff8b7b891
Michal Pietrusinski <michal.pietrusinski@roche.com>
2025-11-18 21:24:54 +0100

    Update MongoDB dependencies and improve test configurations

--- src/test/java/com/ontotext/trree/plugin/mongodb/AbstractMongoTest.java
+++ src/test/java/com/ontotext/trree/plugin/mongodb/AbstractMongoTest.java
@@ -1,54 +1,40 @@
 package com.ontotext.trree.plugin.mongodb;
 
 import com.ontotext.test.functional.base.SingleRepositoryFunctionalTest;
-import de.flapdoodle.embed.mongo.MongodExecutable;
-import de.flapdoodle.embed.mongo.MongodProcess;
-import de.flapdoodle.embed.mongo.MongodStarter;
-import de.flapdoodle.embed.mongo.config.IMongodConfig;
-import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
-import de.flapdoodle.embed.mongo.config.Net;
-import de.flapdoodle.embed.mongo.distribution.Version;
-import de.flapdoodle.embed.process.runtime.Network;
+import de.flapdoodle.embed.mongo.transitions.Mongod;
+import de.flapdoodle.embed.mongo.transitions.RunningMongodProcess;
+import de.flapdoodle.reverse.TransitionWalker;
 import org.junit.After;
 import org.junit.Before;
 
-import java.io.IOException;
-
 /**
- * Class that all Mongo classed should extend in order to get an instance of {@link MongodProcess}
+ * Class that all Mongo classes should extend in order to get an instance of running MongoDB
  */
 public abstract class AbstractMongoTest extends SingleRepositoryFunctionalTest {
 
-	protected MongodProcess mongoProcess;
+	protected RunningMongodProcess mongoProcess;
+	protected TransitionWalker.ReachedState<RunningMongodProcess> running;
 
 	@Before
 	public void setup() {
-		this.mongoProcess = startMongod();
+		this.running = startMongod();
+		this.mongoProcess = running.current();
 	}
 
 	@After
 	public void cleanup() {
-		if (mongoProcess.isProcessRunning()) {
-			mongoProcess.stop();
+		if (running != null) {
+			running.close();
 		}
 	}
 
-	protected static MongodProcess startMongod() {
-		IMongodConfig mongoConfigConfig;
-		MongodProcess mongod = null;
+	protected static TransitionWalker.ReachedState<RunningMongodProcess> startMongod() {
 		try {
-			mongoConfigConfig = new MongodConfigBuilder()
-					.version(Version.V3_5_5)
-					.net(new Net(Network.getFreeServerPort(), Network.localhostIsIPv6()))
-					.build();
-
-			MongodExecutable mongodExecutable = MongodStarter.getDefaultInstance().prepare(mongoConfigConfig);
-			mongod = mongodExecutable.start();
-		} catch (IOException e) {
+			return Mongod.instance().start(de.flapdoodle.embed.mongo.distribution.Version.Main.V4_4);
+		} catch (Exception e) {
 			e.printStackTrace();
+			throw new RuntimeException("Failed to start MongoDB", e);
 		}
-
-		return mongod;
 	}
 
 }
